7260b5e40f74285d4f428b0d74106166cc3a137b
duduita <eduardomenezesm@msn.com>
2021-02-09 11:52:00 -0300

    Add new features

--- netcdf-vis.js
+++ netcdf-vis.js
@@ -19,7 +19,8 @@ function initDemoMap() {
     minZoom: 2,
     maxZoom: 5,
     maxBounds: bounds,
-    maxBoundsViscosity: 1
+    maxBoundsViscosity: 1,
+    attributionControl: false,
   });
 
   // layer control panal
@@ -33,14 +34,14 @@ function initDemoMap() {
     maxZoom: 18,
     cellSize: 5,
     exp: 3,
-    max: 75
+    max: 75,
   });
   //idw.addTo(map);
   //layerControl.addOverlay(idw, "Global Temp").addTo(map);
 
   return {
     map: map,
-    layerControl: layerControl
+    layerControl: layerControl,
   };
 }
 
@@ -50,25 +51,25 @@ var map = mapStuff.map;
 var layerControl = mapStuff.layerControl;
 
 // load data (u, v grids) from somewhere (e.g. https://github.com/danwild/wind-js-server)
-$.getJSON("data/australia.json", function(data) {
+$.getJSON("data/australia.json", function (data) {
   var velocityLayer = L.velocityLayer({
     displayValues: true,
     displayOptions: {
       velocityType: "Wind",
       displayPosition: "bottomleft",
-      displayEmptyString: "No wind data"
+      displayEmptyString: "No wind data",
     },
     data: data,
-    maxVelocity: 25
+    maxVelocity: 25,
   });
 
   layerControl.addOverlay(velocityLayer, "Australia Wind");
 
-  $(document).ready(function() {
+  $(document).ready(function () {
     var on = 0;
     //alert("ready");
     $("#australia")
-      .on("click", function() {
+      .on("click", function () {
         //alert("called");
         if (!on) {
           velocityLayer.addTo(map);
@@ -82,25 +83,25 @@ $.getJSON("data/australia.json", function(data) {
   });
 });
 
-$.getJSON("data/global.json", function(data) {
+$.getJSON("data/global.json", function (data) {
   var velocityLayer = L.velocityLayer({
     displayValues: true,
     displayOptions: {
       velocityType: "Wind",
       displayPosition: "bottomleft",
-      displayEmptyString: "No wind data"
+      displayEmptyString: "No wind data",
     },
     data: data,
-    maxVelocity: 25
+    maxVelocity: 25,
   });
 
   layerControl.addOverlay(velocityLayer, "Global Wind");
 
-  $(document).ready(function() {
+  $(document).ready(function () {
     var on = 0;
     //alert("ready");
     $("#global")
-      .on("click", function() {
+      .on("click", function () {
         //alert("called");
         if (!on) {
           velocityLayer.addTo(map);
@@ -114,26 +115,26 @@ $.getJSON("data/global.json", function(data) {
   });
 });
 
-$.getJSON("data/problem.json", function(data) {
+$.getJSON("data/problem.json", function (data) {
   var velocityLayer = L.velocityLayer({
     displayValues: true,
     displayOptions: {
       velocityType: "Wind",
       displayPosition: "bottomleft",
-      displayEmptyString: "No wind data"
+      displayEmptyString: "No wind data",
     },
     data: data,
-    maxVelocity: 25
+    maxVelocity: 25,
   });
 
   layerControl.addOverlay(velocityLayer, "South America Wind");
   map.setView([-18, -50], 5);
 
-  $(document).ready(function() {
+  $(document).ready(function () {
     var on = 0;
     //alert("ready");
     $("#south")
-      .on("click", function() {
+      .on("click", function () {
         //alert("called");
         if (!on) {
           velocityLayer.addTo(map);
@@ -147,26 +148,26 @@ $.getJSON("data/problem.json", function(data) {
   });
 });
 
-$.getJSON("data/newmodel.json", function(data) {
+$.getJSON("data/newmodel.json", function (data) {
   var velocityLayer = L.velocityLayer({
     displayValues: true,
     displayOptions: {
       velocityType: "Wind",
       displayPosition: "bottomleft",
-      displayEmptyString: "No wind data"
+      displayEmptyString: "No wind data",
     },
     data: data,
-    maxVelocity: 25
+    maxVelocity: 25,
   });
 
   layerControl.addOverlay(velocityLayer, "Sampling America Wind");
   map.setView([-18, -50], 5);
 
-  $(document).ready(function() {
+  $(document).ready(function () {
     var on = 0;
     //alert("ready");
     $("#test")
-      .on("click", function() {
+      .on("click", function () {
         //alert("called");
         if (!on) {
           velocityLayer.addTo(map);
@@ -178,20 +179,67 @@ $.getJSON("data/newmodel.json", function(data) {
       })
       .change();
   });
+});
 
-  $(document).ready(function() {
-    var imageUrl =
-        "https://estatico-redemet.decea.mil.br/satelite/2020/03/21/realcada/maps/realcada_202003211300.png",
-      imageBounds = [[-56, -100], [12.52, -25.24]];
+$(document).ready(function () {
+  $.ajax({
+    url: `https://api-redemet.decea.mil.br/produtos/satelite/realcada?api_key=gdkP7S0gy9sB4JsOLoYe34D52CGyrDzZK3xAWe80&data=2021020911`,
+  }).then(function (res) {
+    //var formattedJson = JSON.stringify(res, null, "\t");
+    //console.log(formattedJson);
+    imageBounds = [
+      [res.data.lat_lon.lat_min, res.data.lat_lon.lon_min],
+      [res.data.lat_lon.lat_max, res.data.lat_lon.lon_max],
+    ];
+    imageUrl = res.data.satelite[0].path;
     var layer = L.imageOverlay(imageUrl, imageBounds);
     var actived = 0;
     $("#satellite")
-      .on("click", function() {
+      .on("click", function () {
         if (!actived) {
           layer.addTo(map);
           actived = 1;
         } else {
           layer.remove(map);
+          actived = 0;
+        }
+      })
+      .change();
+  });
+});
+
+$(document).ready(function () {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const day = today.getDate();
+  const hour = today.getHours();
+  const formattedToday = `${year}${month}${day}${hour}`;
+  alert(formattedToday);
+  $.ajax({
+    url: `https://api-redemet.decea.mil.br/produtos/radar/maxcappi?api_key=gdkP7S0gy9sB4JsOLoYe34D52CGyrDzZK3xAWe80&data=2021020911`,
+  }).then(function (res) {
+    var formattedJson = JSON.stringify(res, null, "\t");
+    var myLayerGroup = L.layerGroup();
+    for (i = 0; i < res.data.radar[0].length; i++) {
+      imageBounds = [
+        [res.data.radar[0][i].lat_min, res.data.radar[0][i].lon_min],
+        [res.data.radar[0][i].lat_max, res.data.radar[0][i].lon_max],
+      ];
+      imageUrl = res.data.radar[0][i].path;
+      if (imageUrl != undefined) {
+        var layer = L.imageOverlay(imageUrl, imageBounds);
+        myLayerGroup.addLayer(layer);
+      }
+    }
+    var actived = 0;
+    $("#radar")
+      .on("click", function () {
+        if (!actived) {
+          myLayerGroup.addTo(map);
+          actived = 1;
+        } else {
+          myLayerGroup.remove(map);
           actived = 0;
         }
       })
